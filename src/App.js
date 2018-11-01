f272e41449ff74eefa548bdb1cd4539773652776
vishshukla <vwshukla@gmail.com>
2018-10-31 23:59:30 -0700

    added components setup and button css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Button from './components/Button'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+        </div>
       </div>
     );
   }
